4b394f4ea7e91ce4ba414b8802fe9722e53145c3
vijay-chopade <vijaychopade9211@gmail.com>
2022-11-16 11:39:21 +0530

    shivasir|vijay|L9.1|get data using SQL|Query class|11-11-2022

--- project_lecture_api/src/main/java/com/example/apiex/QuestionController.java
+++ project_lecture_api/src/main/java/com/example/apiex/QuestionController.java
@@ -2,8 +2,7 @@ package com.example.apiex;
 
 import java.util.List;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
+import org.hibernate.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +14,36 @@ public class QuestionController {
 	@Autowired
 	SessionFactory factory;
 	
+	@RequestMapping("getall")
+	public List<Object[]> getAllData() {
+		Session session = factory.openSession();
+		Query query = session.createSQLQuery("select question.question_id qid,question ,answer_id aid,answer from question join answer on question.question_id = answer.question_id" );
+		List<Object[]> list = query.list();
+		return list;
+	}
+	
+	
+
+	@RequestMapping("answer/{id}")
+	public List<Object[]> getAnswer(@PathVariable int id) {
+		Session session = factory.openSession();
+		session.load(Answer.class, id);
+		//Query query = session.createSQLQuery(
+				//"select question.question_id qid,question ,answer_id aid,answer from question join answer on question.question_id = "+id+" and answer.question_id = "
+				//		+ id); // answer with question
+		Query query = session.createSQLQuery(
+				"select answer_id aid,answer from question join answer on question.question_id = "+id+" and answer.question_id = "
+						+ id); // set question number and get only answers
+		List<Object[]> list = query.list();
+//		for (Object[] objects : list) {
+//			for (Object objects2 : objects) {
+//				System.out.println(objects2);
+//			}
+//
+//		}
+		return list;
+	}
+
 	@RequestMapping("question/{id}")
 	public Question getQuestion(@PathVariable int id) {
 		Session session = factory.openSession();
@@ -22,5 +51,5 @@ public class QuestionController {
 		List<Answer> answer = question.getAnswers();
 		return question;
 	}
-	
+
 }
--- project_lecture_api/src/main/java/com/example/project_lecture_api/ProjectLectureApiApplication.java
+++ project_lecture_api/src/main/java/com/example/project_lecture_api/ProjectLectureApiApplication.java
@@ -15,5 +15,6 @@ public class ProjectLectureApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectLectureApiApplication.class, args);
 	}
-
-}
+  
+}  
+  
